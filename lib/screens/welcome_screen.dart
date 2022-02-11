ecc770de5488f52d48a8570a424b488f0882ac9a
pb3enguin <pb3enguin@gmail.com>
2022-02-12 01:44:39 +0900

    ?

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -27,6 +27,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     controller.forward();
 
     controller.addListener(() {
+      setState(() {});
       print(controller.value);
     });
   }
@@ -43,12 +44,9 @@ class _WelcomeScreenState extends State<WelcomeScreen>
           children: <Widget>[
             Row(
               children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    child: Image.asset('images/logo.png'),
-                    height: 60.0,
-                  ),
+                Container(
+                  child: Image.asset('images/logo.png'),
+                  height: 60.0,
                 ),
                 const Text(
                   'Flash Chat',
